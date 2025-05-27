d4e478a3b9b191701c241a6d4503d2905e12b7c1
Md Faizan <mdfaizankurisaray@gmail.com>
2025-05-27 15:14:17 +0530

    first commit

---
+++ code1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=10; i++) {
+        cout << i <<endl;
+    }
+    cout << endl;
+
+
+
+
+
+
+    for(int i=0; i<5; i++) {
+        cout << "Apna college" <<endl;
+    }
+
+     cout << endl;
+
+
+    return 0;
+}
+
+
