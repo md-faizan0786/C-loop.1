da8672fa3c9ee00e1986141cd40f42654081a5cb
Md Faizan <mdfaizankurisaray@gmail.com>
2025-05-29 13:37:44 +0530

    5th commit

---
+++ code5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int val = 1;
+
+    do {
+        cout << "apna college in do-while loop" <<endl;
+    } while(val > 5);
+
+
+    while (val > 5) {
+        cout<< "apna college in while loop";
+    }
+    
+    return 0;
+}
